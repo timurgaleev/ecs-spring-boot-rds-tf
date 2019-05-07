90ac0cd76ab7c625db5ae2fc4352200badd2c682
Timur <aksis@main-server.AksisLAN>
2019-05-07 18:33:16 +0000

    change tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "eu-west-2"
+  region     = "eu-central-1"
 }
 data "aws_ami" "sp_app_ami" {
   most_recent = true
