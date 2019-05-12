fbd3cd9a9f928de1874b6c2582b702fdecd4e45b
Timur <aksis@main-server.AksisLAN>
2019-05-12 14:49:52 +0000

    change java app

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "provectus_app:latest"
+  default     = "035898547283.dkr.ecr.us-west-2.amazonaws.com/provectus_app:latest"
 }
 
 variable "app_port" {
